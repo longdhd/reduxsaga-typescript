56b50c0f11eb533e9e340da366b197a05a41d3fd
LongDang <long.danghoangd@gmail.com>
2023-04-11 22:12:55 +0700

    dashboard saga

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,4 +1,27 @@
+import { useAppDispatch, useAppSelector } from "app/hooks"
+import { useEffect } from "react"
+import { dashboardAction, selectHighestMarkList, selectLoading, selectLowestMarkList, selectRankingByCity, selectStats } from "./dashboardSlice";
+
 export function Dashboard() {
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector(selectLoading);
+  const stats = useAppSelector(selectStats);
+  const highestMarkList = useAppSelector(selectHighestMarkList);
+  const lowestMarkList = useAppSelector(selectLowestMarkList);
+  const rankingByCity = useAppSelector(selectRankingByCity);
+
+  console.log({
+    loading,
+    stats,
+    highestMarkList,
+    lowestMarkList,
+    rankingByCity
+  })
+
+  useEffect(() => {
+    dispatch(dashboardAction.fetchData());
+    
+  },[])
   return (
     <div>
         Dashboard
