9d94d66cdae851207fa38eda8e5a55b11b3ea7c3
LongDang <long.danghoangd@gmail.com>
2023-04-11 10:44:33 +0700

    customRouter compatiable with react router dom v6

---
+++ src/components/Common/CustomRouter.tsx
@@ -0,0 +1,26 @@
+import { useLayoutEffect, useState } from "react";
+import { BrowserRouterProps, Router } from "react-router-dom";
+import { BrowserHistory } from "history";
+import customHistory from "../../app/history";
+
+interface Props extends BrowserRouterProps {
+  history: BrowserHistory;
+}
+const CustomRouter = ({ basename, history, children }: Props) => {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+  useLayoutEffect(() => history.listen(setState), [history]);
+  return (
+    <Router
+      navigator={customHistory}
+      location={state.location}
+      navigationType={state.action}
+      children={children}
+      basename={basename}
+    />
+  );
+};
+
+export default CustomRouter;
